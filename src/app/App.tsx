b68c481b449da7bbc1ed776a17df643aa5d54428
Ganiev Rustam <ganiev.rystam@gmail.com>
2024-05-27 16:59:30 +0300

    check

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,6 +4,7 @@ import './styles/index.scss'
 export const App = () => {
   return (
     <div className='app dark'>
+      <div>ROBOT</div>
       <Navbar/>
         <GenerationPassword />
     </div>
